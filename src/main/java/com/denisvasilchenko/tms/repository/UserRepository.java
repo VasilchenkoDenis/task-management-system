f08aec69e5624779cd70646789969e6841388835
Denis <virgilion1990@gmail.com>
2025-01-21 23:58:01 +0300

    feat: add search by username and email

--- src/main/java/com/denisvasilchenko/tms/repository/UserRepository.java
+++ src/main/java/com/denisvasilchenko/tms/repository/UserRepository.java
@@ -2,6 +2,15 @@ package com.denisvasilchenko.tms.repository;
 
 import com.denisvasilchenko.tms.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
