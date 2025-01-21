a7bbfccca08e0e8c9d0327fa3cf9f39b7b294156
Denis <virgilion1990@gmail.com>
2025-01-21 23:55:32 +0300

    refactor: add annotations

--- src/main/java/com/denisvasilchenko/tms/repository/TaskRepository.java
+++ src/main/java/com/denisvasilchenko/tms/repository/TaskRepository.java
@@ -1,6 +1,9 @@
 package com.denisvasilchenko.tms.repository;
 
+import com.denisvasilchenko.tms.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TaskRepository {
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
