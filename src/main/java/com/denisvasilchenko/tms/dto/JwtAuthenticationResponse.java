3d152c748423e354c13ad31273cee7d3b93cb22e
Denis <virgilion1990@gmail.com>
2025-01-22 00:06:38 +0300

    feat: add JWT response

---
+++ src/main/java/com/denisvasilchenko/tms/dto/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.denisvasilchenko.tms.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Ответ c токеном доступа")
+public class JwtAuthenticationResponse {
+    @Schema(description = "Токен доступа", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY...")
+    private String token;
+}
