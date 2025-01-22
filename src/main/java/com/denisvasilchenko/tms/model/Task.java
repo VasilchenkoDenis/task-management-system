5366ed05f1d151dce42f5ec77131925e7acccb0d
Denis <virgilion1990@gmail.com>
2025-01-23 01:37:44 +0300

    refactor: change field "comment" type

--- src/main/java/com/denisvasilchenko/tms/model/Task.java
+++ src/main/java/com/denisvasilchenko/tms/model/Task.java
@@ -1,10 +1,20 @@
 package com.denisvasilchenko.tms.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,10 +25,11 @@ public class Task {
     private TaskStatus taskStatus;
     @Enumerated(EnumType.STRING)
     private Priority priority;
-    private String comment;
     @ManyToOne
     private User author;
     @ManyToOne
     private User assignee;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
 
 }
