d56705942cc028d44743cc65db1187d4d6a7aa06
Denis <virgilion1990@gmail.com>
2025-01-23 01:39:33 +0300

    feat: add getAllUsers function with pagination

--- src/main/java/com/denisvasilchenko/tms/controller/UserController.java
+++ src/main/java/com/denisvasilchenko/tms/controller/UserController.java
@@ -1,4 +1,49 @@
 package com.denisvasilchenko.tms.controller;
 
+
+import com.denisvasilchenko.tms.dto.user.UserResponse;
+import com.denisvasilchenko.tms.dto.user.UserResponseConverter;
+import com.denisvasilchenko.tms.model.User;
+import com.denisvasilchenko.tms.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@EnableSpringDataWebSupport
+@AllArgsConstructor
+@Tag(name = "User API")
+@RequestMapping("/api/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserResponseConverter userResponseConverter;
+
+
+    @GetMapping()
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Page<UserResponse>> getAllUsers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<User> userPage = userService.findAll(pageable);
+        Page<UserResponse> users = userPage.map(userResponseConverter::convertToResponse);
+
+        return ResponseEntity.ok(users);
+    }
 }
