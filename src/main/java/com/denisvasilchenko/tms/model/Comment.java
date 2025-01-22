ed9571268b65610813abd3515ed770304156592f
Denis <virgilion1990@gmail.com>
2025-01-23 01:41:10 +0300

    refactor

--- src/main/java/com/denisvasilchenko/tms/model/Comment.java
+++ src/main/java/com/denisvasilchenko/tms/model/Comment.java
@@ -2,10 +2,12 @@ package com.denisvasilchenko.tms.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
