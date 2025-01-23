5660878b2a07f42a5295d53f45d3b8130ecc7ae0
Denis <virgilion1990@gmail.com>
2025-01-24 00:26:59 +0300

    feat: pagination and filters

--- src/main/java/com/denisvasilchenko/tms/repository/TaskRepository.java
+++ src/main/java/com/denisvasilchenko/tms/repository/TaskRepository.java
@@ -2,8 +2,9 @@ package com.denisvasilchenko.tms.repository;
 
 import com.denisvasilchenko.tms.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
 }
