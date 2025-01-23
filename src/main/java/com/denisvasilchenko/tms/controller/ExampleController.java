d104e3ef892240ae95bc30e40469c13d5b2ef8ae
Denis <virgilion1990@gmail.com>
2025-01-24 00:25:55 +0300

    refactor: add dto mapping

--- src/main/java/com/denisvasilchenko/tms/controller/ExampleController.java
+++ src/main/java/com/denisvasilchenko/tms/controller/ExampleController.java
@@ -1,9 +1,14 @@
 package com.denisvasilchenko.tms.controller;
 
+import com.denisvasilchenko.tms.dto.EntityMapper;
+import com.denisvasilchenko.tms.dto.user.UserResponse;
+import com.denisvasilchenko.tms.model.User;
 import com.denisvasilchenko.tms.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Аутентификация")
 public class ExampleController {
     private final UserService service;
+
+    @Autowired
+    private EntityMapper entityMapper;
 
     @GetMapping
     @Operation(summary = "Доступен только авторизованным пользователям")
@@ -31,7 +39,9 @@ public class ExampleController {
 
     @GetMapping("/get-admin")
     @Operation(summary = "Получить роль ADMIN (для демонстрации)")
-    public void getAdmin() {
-        service.getAdmin();
+    public ResponseEntity<UserResponse> getAdmin() {
+        User user = service.getAdmin();
+        UserResponse userResponse = entityMapper.userToResponse(user);
+        return ResponseEntity.ok(userResponse);
     }
 }
