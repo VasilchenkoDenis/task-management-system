7cfe35b75abc0c52c60d56ae33abfd5ab9a23c56
Denis <virgilion1990@gmail.com>
2025-01-27 17:30:54 +0300

    feat: add filtration and pagination

--- src/main/java/com/denisvasilchenko/tms/repository/CommentRepository.java
+++ src/main/java/com/denisvasilchenko/tms/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package com.denisvasilchenko.tms.repository;
 
 import com.denisvasilchenko.tms.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
 }
