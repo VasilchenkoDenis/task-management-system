89705b4c4b2d35aa187ce2a82365f53175812df9
Denis <virgilion1990@gmail.com>
2025-01-27 17:41:59 +0300

    feat: add demo user

--- src/main/java/com/denisvasilchenko/tms/TmsApplication.java
+++ src/main/java/com/denisvasilchenko/tms/TmsApplication.java
@@ -1,13 +1,35 @@
 package com.denisvasilchenko.tms;
 
+import com.denisvasilchenko.tms.model.User;
+import com.denisvasilchenko.tms.security.service.JwtService;
+import com.denisvasilchenko.tms.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TmsApplication.class, args);
+		SpringApplication.run(TmsApplication.class, args);}
+
+		@Bean
+		public CommandLineRunner commandLineRunner(
+				UserService userService,
+				JwtService jwtService
+	) {
+			return args -> {
+				User user = User.builder()
+						.email("default_user@mail.test")
+						.password("default-user-password")
+						.username("Default User")
+						.build();
+				try {
+					user=userService.save(user);
+				} catch (Exception ignored){}
+				System.out.println("Demo user token: " + jwtService.generateToken(user));
+			};
+		}
 	}
 
-}
