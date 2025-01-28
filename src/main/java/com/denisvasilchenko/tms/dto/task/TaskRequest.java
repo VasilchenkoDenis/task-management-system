63b0c45f0badeca05d086eae5f0bba8d075f7659
Denis <virgilion1990@gmail.com>
2025-01-29 02:12:49 +0300

    refactor: add fields and change fields type

--- src/main/java/com/denisvasilchenko/tms/dto/task/TaskRequest.java
+++ src/main/java/com/denisvasilchenko/tms/dto/task/TaskRequest.java
@@ -1,5 +1,7 @@
 package com.denisvasilchenko.tms.dto.task;
 
+import com.denisvasilchenko.tms.model.Comment;
+import com.denisvasilchenko.tms.model.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -7,18 +9,22 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class TaskRequest {
+    private long id;
     @NotBlank (message = "Title must not be empty")
     @NotNull (message = "Title must not be empty")
     private String title;
     private String description;
     private String taskStatus;
     private String priority;
-    private String assigneeEmail;
-    private Long assigneeId;
+    private User assignee;
+    private User author;
+    private List<Comment> comments;
 }
