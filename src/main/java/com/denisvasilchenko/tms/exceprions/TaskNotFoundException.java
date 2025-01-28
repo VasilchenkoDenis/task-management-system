67143cc2483aaf9731066c9847fa107cfeb7a437
Denis <virgilion1990@gmail.com>
2025-01-29 02:12:08 +0300

    refactor:

--- src/main/java/com/denisvasilchenko/tms/exceprions/TaskNotFoundException.java
+++ src/main/java/com/denisvasilchenko/tms/exceprions/TaskNotFoundException.java
@@ -4,4 +4,8 @@ public class TaskNotFoundException extends RuntimeException{
     public TaskNotFoundException(Long id){
         super("Task with ID "+id+" not found");
     }
+
+    public TaskNotFoundException(String message){
+        super(message);
+    }
 }
