1272b0385c37c52b365c254f10b685bb825a58f8
Denis <virgilion1990@gmail.com>
2025-01-29 02:06:53 +0300

    refactor: add exception handler

--- src/main/java/com/denisvasilchenko/tms/controller/GlobalExceptionHandler.java
+++ src/main/java/com/denisvasilchenko/tms/controller/GlobalExceptionHandler.java
@@ -44,4 +44,9 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
 }
